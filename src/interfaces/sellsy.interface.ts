27ac3aa3de60ddd86f973723ee38d33f44f53585
Mr-Fougere <alexandre7822@gmail.com>
2024-10-04 18:26:01 +0200

    :art: Prettier

--- src/interfaces/sellsy.interface.ts
+++ src/interfaces/sellsy.interface.ts
@@ -1,20 +1,19 @@
 export interface DocumentResult {
-	status: string;
-	response: {
-		infos: any; 
-		result: any;
-    status: string
-	};
+  status: string;
+  response: {
+    infos: any;
+    result: any;
+    status: string;
+  };
 }
 
 export interface PeriodDates {
-	start: Date;
-	end: Date;
+  start: Date;
+  end: Date;
 }
-
 
 export interface DocumentsOutput {
   infos: any;
   documents: any;
-  status: string 
+  status: string;
 }
