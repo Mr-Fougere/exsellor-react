843f78b8216a1864b555da9adc57331727f7ebed
Mr-Fougere <alexandre7822@gmail.com>
2024-10-04 22:31:13 +0200

    :art: Add year in month selector

--- src/components/MonthSelector.tsx
+++ src/components/MonthSelector.tsx
@@ -10,6 +10,9 @@ type MonthSelectorProps = {
 
 const MonthSelector = ({ setDates, selectedDates }: MonthSelectorProps) => {
   const [months, setMonths] = useState<{ [key: number]: string }>({});
+  const [selectedYear, setSelectedYear] = useState<number>(
+    new Date().getFullYear()
+  );
 
   const monthNames = [
     "Janvier",
@@ -31,11 +34,13 @@ const MonthSelector = ({ setDates, selectedDates }: MonthSelectorProps) => {
   const currentYear = today.getFullYear();
 
   const getFirstAndLastDays = (month: number) => {
-    const firstDay = new Date(currentYear as number, month, 1);
+    const firstDay = new Date(selectedYear, month, 1);
     const lastDay = new Date(
-      currentYear as number,
-      month + (month == currentMonth ? 0 : 1),
-      month == currentMonth ? today.getDay() - 1 : 0
+      selectedYear,
+      month + (month === currentMonth && selectedYear === currentYear ? 0 : 1),
+      month === currentMonth && selectedYear === currentYear
+        ? today.getDate()
+        : 0
     );
     return { start: firstDay, end: lastDay };
   };
@@ -46,40 +51,78 @@ const MonthSelector = ({ setDates, selectedDates }: MonthSelectorProps) => {
     const { start, end } = getFirstAndLastDays(month);
 
     const areStartTimesEqual =
-      selectedDates.start.getDay() === start.getDay() &&
-      selectedDates.start.getMonth() === start.getMonth();
+      selectedDates.start.getDate() === start.getDate() &&
+      selectedDates.start.getMonth() === start.getMonth() &&
+      selectedDates.start.getFullYear() === selectedYear;
 
     const areEndTimesEqual =
-      selectedDates.end.getDay() === end.getDay() &&
-      selectedDates.end.getMonth() === end.getMonth();
+      selectedDates.end.getDate() === end.getDate() &&
+      selectedDates.end.getMonth() === end.getMonth() &&
+      selectedDates.end.getFullYear() === selectedYear;
 
     return areStartTimesEqual && areEndTimesEqual;
   };
 
   useEffect(() => {
     const availableMonths: Record<string, string> = {};
-    for (let i = 0; i < currentMonth + 1; i++) {
+    for (
+      let i = 0;
+      i < (currentYear === selectedYear ? currentMonth + 1 : 12);
+      i++
+    ) {
       availableMonths[i] = monthNames[i];
     }
     setMonths(availableMonths);
-  }, []);
+  }, [selectedYear]); // Update months when the selected year changes
+
+  const handlePrevYear = () => {
+    console.log(selectedYear, currentYear);
+
+    if (selectedYear > 2013) {
+      console.log(selectedYear - 1);
+
+      setSelectedYear(selectedYear - 1);
+    }
+  };
+
+  const handleNextYear = () => {
+    if (selectedYear < currentYear) {
+      setSelectedYear(selectedYear + 1);
+    }
+  };
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4 px-2">
-      {Object.entries(months).map(([monthIndex, monthName]) => (
-        <button
-          type="button"
-          key={monthIndex}
-          onClick={() => setDates(getFirstAndLastDays(Number(monthIndex)))}
-          className={`rounded px-4 py-2 hover:bg-sky-200 border border-gray-300 rounded ${
-            isSelected(Number(monthIndex))
-              ? "bg-cyan-600 text-white"
-              : "bg-sky-50 text-black"
-          }`}
-        >
-          {monthName} {currentYear}
-        </button>
-      ))}
+    <div className="flex flex-row items-center">
+      <div
+        onClick={handlePrevYear}
+        className="px-2 py-1 border border-gray-300 rounded hover:bg-gray-200 cursor:pointer"
+      >
+        <i className="fa-solid fa-chevron-left"></i>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4 px-2">
+        {Object.entries(months).map(([monthIndex, monthName]) => (
+          <button
+            type="button"
+            key={monthIndex}
+            onClick={() => setDates(getFirstAndLastDays(Number(monthIndex)))}
+            className={`rounded px-4 py-2 hover:bg-sky-200 border border-gray-300 rounded ${
+              isSelected(Number(monthIndex))
+                ? "bg-cyan-600 text-white"
+                : "bg-sky-50 text-black"
+            }`}
+          >
+            {monthName} {selectedYear}
+          </button>
+        ))}
+      </div>
+
+      <div
+        onClick={handleNextYear}
+        className="px-2 py-1 border border-gray-300 rounded hover:bg-gray-200 cursor:pointer"
+      >
+        <i className="fa-solid fa-chevron-right"></i>
+      </div>
     </div>
   );
 };
